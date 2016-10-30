c9f28de5d1fa63f6a59fd52ca67e16dd963e90b5
Juni <junishop@gmail.com>
2016-10-30 11:03:42 +0900

    sitemap

--- www/views/part/footer.php
+++ www/views/part/footer.php
@@ -1,40 +1,40 @@
 <!-- Footer.html -->
-		</div>
-	</div>
-	<div id="footer">
-		<div class="container">
-			<div class="wrap">
-				<div class="inb">
-					<ul>
-						<li><a href="#">창업지원단 소개</a></li>
-						<li><a href="#">시설안내</a></li>
-						<li><a href="#">개인정보처리방침</a></li>
-						<li><a href="#">이메일주소무단수집거부</a></li>
-						<li><a href="#">찾아오시는길</a></li>
-					</ul>
-				</div>
-				<div class="sns">
-					<ul>
-						<li class="twitter"><a href="#"><span class="sr-only">트위터</span></a></li>
-						<li class="facebook"><a href="#"><span class="sr-only">페이스북</span></a></li>
-					</ul>
-				</div>
+</div>
+</div>
+<div id="footer">
+	<div class="container">
+		<div class="wrap">
+			<div class="inb">
+				<ul>
+					<li><a href="#">창업지원단 소개</a></li>
+					<li><a href="#">시설안내</a></li>
+					<li><a href="#">개인정보처리방침</a></li>
+					<li><a href="#">이메일주소무단수집거부</a></li>
+					<li><a href="#">찾아오시는길</a></li>
+				</ul>
 			</div>
-			<div class="wrap">
-				<p class="contact">21999 인천광역시 연수구 갯벌로 송도테크노파크 미추홀타워 별관A동 504호 인천대학교 창업지원단 &nbsp; Tel : 032-835-9683 &nbsp; Fax : 032-835-4161</p>
-				<p class="copyright">Copyright (c) 2016 Incheon National University. All Rights Reserved.</p>
-				<div class="familysite">
-					<a href="#familysite">관련사이트 바로가기</a>
-					<ul>
-						<li><a href="#">사이트1</a></li>
-						<li><a href="#">사이트2</a></li>
-						<li><a href="#">사이트3</a></li>
-					</ul>
-				</div>
+			<div class="sns">
+				<ul>
+					<li class="twitter"><a href="#"><span class="sr-only">트위터</span></a></li>
+					<li class="facebook"><a href="#"><span class="sr-only">페이스북</span></a></li>
+				</ul>
 			</div>
 		</div>
+		<div class="wrap">
+			<p class="contact">21999 인천광역시 연수구 갯벌로 송도테크노파크 미추홀타워 별관A동 504호 인천대학교 창업지원단 &nbsp; Tel : 032-835-9683 &nbsp; Fax : 032-835-4161</p>
+			<p class="copyright">Copyright (c) 2016 Incheon National University. All Rights Reserved.</p>
+			<div class="familysite">
+				<a href="#familysite">관련사이트 바로가기</a>
+				<ul>
+					<li><a href="#">사이트1</a></li>
+					<li><a href="#">사이트2</a></li>
+					<li><a href="#">사이트3</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
-	<div class="header-over-bg"></div>
+</div>
+<div class="header-over-bg"></div>
 </div>
 
 <div class="sitemap">
@@ -42,9 +42,11 @@
 	<div class="cont">
 		<div>
 			<h2>
+				<span class="sr-only">
 				<strong>인천대학교 창업지원단<br>
-					전체메뉴 보기</strong>
+					전체메뉴</strong>
 				<span class="hidden-xs"></em></span>
+				</span>
 			</h2>
 			<div>
 				<ul>
@@ -187,7 +189,3 @@
 	<a href="#close-sitemap" class="close-sitemap"><span class="sr-only">사이트맵 닫기</span></a> </div>
 
 <!-- //Footer.html -->
-<div class="lnb">
-	<h2 class="sr-only">메인메뉴</h2>
-
-</div>

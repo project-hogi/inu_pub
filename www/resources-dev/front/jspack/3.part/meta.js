edf4c46a2c543f4493d5fe527753aa11078ede4a
조용준 <junishop@gmail.com>
2016-11-29 14:55:57 +0900

    asd

--- www/resources-dev/front/jspack/3.part/meta.js
+++ www/resources-dev/front/jspack/3.part/meta.js
@@ -19,3 +19,40 @@ if (desktopModeTF == "true") {
 } else {
     document.write('<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">')
 }
+
+
+
+(function($) {
+    $(document).on("appinit", function() {
+        $(window).resize(function(){
+            if($(window).width() < 1024){
+                if (desktopModeTF != "true") {
+                    $(".mode-btn").html(
+                        '<button onclick="desktopMode()" class="mode-change mobile">PC버전</button>'
+                    );
+                }else{
+                    $(".mode-btn").html(
+                        '<button onclick="undesktopMode()" class="mode-change pc">원래대로</button>'
+                    );
+                }
+            }else{
+                if(desktopModeTF == "true"){
+                    $(".mode-btn").html(
+                        '<button onclick="undesktopMode()" class="mode-change pc">원래대로</button>'
+                    );
+                }
+            }
+        });
+    });
+})(jQuery);
+
+function desktopMode() {
+    document.cookie="DesktopMode=true";
+    document.cookie="DesktopModeScale="+$('html').width()/1024;
+    window.scrollTo(0,0);
+    location.reload();
+}
+function undesktopMode() {
+    document.cookie="DesktopMode=false";
+    location.reload();
+}
--- www/views/part/app-footer.php
+++ www/views/part/app-footer.php
@@ -1,20 +1,6 @@
-<script>if (desktopModeTF == "true") {
-	document.write('<button onclick="undesktopMode()" class="mode-change pc">원래대로</button>');
-}else{
-	document.write('<p onclick="desktopMode()" class="mode-change mobile">PC버전</p>');
-}</script>
-<script>
-	function desktopMode() {
-		document.cookie="DesktopMode=true";
-		document.cookie="DesktopModeScale="+$('html').width()/1024;
-		window.scrollTo(0,0);
-		location.reload();
-	}
-	function undesktopMode() {
-		document.cookie="DesktopMode=false";
-		location.reload();
-	}
-</script>
+<div class="mode-btn"></div>
+
+
 <!-- App Footer.html -->
 </body>
 </html>
